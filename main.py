f28dc794b209589e008d242b63ee294042a2ee42
Juan Carlos Martínez Sevilla <jc@mbp.local>
2023-05-13 11:41:51 +0200

    feat: new model added and endpoint to predict

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, jsonify
 import os
+import tensorflow as tf
+
+da_model = tf.keras.models.load_model('da.h5')
 
 app = Flask(__name__)
 
+@app.route('/document_analysis', methods=['GET'])
+def document_analysis():
+    return jsonify({"bounding_box": "This is a bounding box"})
 
 @app.route('/')
 def index():
